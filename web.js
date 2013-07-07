845b4be725457eec8138a7d492b149b40f68e1d1
Ubuntu <ubuntu@ip-172-31-19-67.us-west-2.compute.internal>
2013-07-07 08:48:25 +0000

    Changes to web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var contents = fs.readFileSync(index.html);
+var contents = fs.readFileSync("index.html");
 
 var buffer = new Buffer(contents);
 
