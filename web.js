d050973201d198a479f3cfa17162edac6cfc0bc2
Ubuntu <ubuntu@ip-172-31-19-67.us-west-2.compute.internal>
2013-07-07 08:56:25 +0000

    Adding require statement to web.js

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 var contents = fs.readFileSync("index.html");
 
 var buffer = new Buffer(contents);
